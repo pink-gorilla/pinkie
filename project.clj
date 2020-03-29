bf4c684fe7fcd706f2844133f67a774885b19320
CI/CD <ci@pinkgorilla.org>
2020-03-29 03:03:16 +0000

    Release 0.1.20

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-renderable-ui "0.1.20-SNAPSHOT"
+(defproject org.pinkgorilla/gorilla-renderable-ui "0.1.20"
   :description "Gorilla Renderable UI - cljs based rendering for Pink Gorilla Notebook."
   :url "https://github.com/pink-gorilla/gorilla-renderable-ui"
   :license {:name "MIT"}
