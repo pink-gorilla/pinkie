38b3a386638b1e5462ca309fd78e6e95bec6e481
CI/CD <ci@pinkgorilla.org>
2020-05-31 23:28:09 +0000

    Begin 0.1.36-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/gorilla-renderable-ui "0.1.35"
+(defproject org.pinkgorilla/gorilla-renderable-ui "0.1.36-SNAPSHOT"
   :description "Gorilla Renderable UI - cljs based rendering for Pink Gorilla Notebook."
   :url "https://github.com/pink-gorilla/gorilla-renderable-ui"
   :license {:name "MIT"}
