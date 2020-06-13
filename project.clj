79f8d6242bad954895abc3fac16628cabd29cd95
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-06-13 13:14:33 -0500

    renderable cljs moved to renderable

--- project.clj
+++ project.clj
@@ -21,28 +21,25 @@
 
   :dependencies [;; [org.clojure/clojure "1.10.1"]
                  ;; [org.clojure/clojurescript "1.10.520"]
-                 [org.pinkgorilla/gorilla-renderable "3.0.5"]
-                 [reagent "0.10.0"
-                  :exclusions [org.clojure/tools.reader
-                               cljsjs/react
-                               cljsjs/react-dom]]
                  ; awb99: adding timbre logging here would fuck up the kernel-shadowdeps bundle compilation.
                  ;[com.taoensso/timbre "4.10.0"]             ; clojurescript logging
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"] ;; awb99: in encoding, and clj/cljs proof
-                 ; re-com uses core.async; therefore we cannot include it in self-hosted clojurescript bundles
-                 ; widgets that use re-com are in notebook
-                 ;[re-com "2.6.0"]      ; reagent reuseable ui components
                  ]
 
   ;; :source-paths ["src"]
   ;; :test-paths ["test"]
-  :plugins [[lein-shell "0.5.0"]]
 
-  :profiles {:dev {:dependencies [[thheller/shadow-cljs "2.8.80"]
+  :profiles {:dev {:dependencies [[reagent "0.10.0"; reagent is devonly, so clients do not need to manage reagent version
+                                   :exclusions [org.clojure/tools.reader
+                                                cljsjs/react
+                                                cljsjs/react-dom]]
+
+                                  [thheller/shadow-cljs "2.8.80"]
                                   [thheller/shadow-cljsjs "0.0.21"]
                                   [clj-kondo "2019.11.23"]]
                    :plugins      [[lein-cljfmt "0.6.6"]
-                                  [lein-cloverage "1.1.2"]]
+                                  [lein-cloverage "1.1.2"]
+                                  [lein-shell "0.5.0"]]
                    :aliases      {"clj-kondo"
                                   ["run" "-m" "clj-kondo.main"]
 
