2a6056da65b10b14f3b4f29a21078e46ca34d91d
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-07-05 21:58:54 -0500

    size selector move in

--- src/pinkie/box.cljs
+++ src/pinkie/box.cljs
@@ -21,3 +21,32 @@
   (let [s (merge (container-style box) opts)]
     ;(println "merged style: " s "box: " box " opts:" opts)
     s))
+
+#_(defn size-selector [size text current set-size!]
+    (let [bg (if (= size current)
+               "bg-green-400"
+               "bg-yellow-400")]
+      [:span.border-solid.ml-2
+       {:class bg
+        :on-click #(set-size! size)} text]))
+
+#_(defn box [{:keys [size render-fn box-fn data]}]
+    (let [size-a (r/atom (or size :small))
+          set-size! (fn [size-new]
+                      (println "size: " size-new)
+                      (reset! size-a size-new))
+         ;handle (useFullScreenHandle)  ; hooks need :f> -  https://github.com/reagent-project/reagent/blob/master/doc/ReactFeatures.md
+         ;_ (println "handle: " handle)
+          ]
+      (fn [{:keys [size render-fn box-fn data]}]
+        (let [style (container-style @size-a)
+              box-fn (or box-fn box-fn-default)]
+          [:div
+           [:span
+            [size-selector :small "sm" @size-a set-size!]
+            [size-selector :medium "md" @size-a set-size!]
+            [size-selector :large "lg" @size-a set-size!]
+            #_[size-selector :full "fs" @size-a set-size!]]
+           [:div.bg-blue-300.overflow-hidden  ; in case the renderer ignores our size
+            style
+            [render-fn (box-fn data style)]]]))))
