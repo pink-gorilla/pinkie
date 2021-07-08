d3afde7dbabb87031ee37238218dfe8a79a1a767
CI/CD <ci@pinkgorilla.org>
2021-07-08 22:07:07 +0000

    Begin 0.3.12-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/pinkie "0.3.11"
+(defproject org.pinkgorilla/pinkie "0.3.12-SNAPSHOT"
   :description "Pinkie reagent component registry."
   :url "https://github.com/pink-gorilla/pinkie"
   :license {:name "MIT"}
