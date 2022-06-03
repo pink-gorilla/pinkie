b027d33d35a3eb424cca980eaeca1a92eaa17d1b
CI/CD <ci@pinkgorilla.org>
2022-06-03 07:47:27 +0000

    Release 0.3.16

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/pinkie "0.3.16-SNAPSHOT"
+(defproject org.pinkgorilla/pinkie "0.3.16"
   :description "Pinkie reagent component registry."
   :url "https://github.com/pink-gorilla/pinkie"
   :license {:name "MIT"}
